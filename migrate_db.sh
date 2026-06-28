d3e74e2585ce9fe4553c9ba7bee9728c788349f3
ahsansabeeh910 <ahsansabeeh910@gmail.com>
2026-06-28 10:53:15 +0530

    chore: Remove migration script containing credentials

--- migrate_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-echo "Backing up from Atlas..."
-mongodump --uri="mongodb+srv://ahsansabeeh910_db_user:c3QohyiOGV13CFoh@cluster0.qfsl8ny.mongodb.net/medical-appointment" --out="$HOME/mongo_backup"
-
-echo "Restoring to new EC2 DB Server..."
-mongorestore --host=172.31.39.24 --port=27017 "$HOME/mongo_backup/"
-
-echo "Migration Complete!"
